8e13adef489aa81dd94f79bab4bbe75cf2f38e89
Leo-Mun <mon04@naver.com>
2017-01-06 14:39:09 +0900

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,30 @@
+package datastructure.studt.dalpha;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+	
+	public static int fibonacci (int n) {
+		if (n == 0) {
+			return 0;
+		}
+		
+		else if (n == 1) {
+			return 1;
+		}
+		
+		else {
+			return fibonacci(n-1) + fibonacci(n-2);
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		
+		System.out.println(fibonacci(num));
+		
+	}
+}
